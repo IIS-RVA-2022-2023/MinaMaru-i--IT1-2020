b3a926882631d453e1c9a68bc57e3202c7f26ce5
minamarusic <minamarusic@192.168.1.3>
2023-03-18 19:04:35 +0100

    3Vezbe-gotovee

--- TuristickaAgencija/src/main/resources/data.sql
+++ TuristickaAgencija/src/main/resources/data.sql
@@ -88,41 +88,56 @@ INSERT INTO "hotel"("id", "naziv", "broj_zvezdica", "opis", "destinacija")
 VALUES (nextval('hotel_seq'), 'Exe Vienna', '3', 'Moderan hotel u lezarnom objektu u kojem se nudi najbolja usluga',6);
 
 
+-------------------------TABELA ARANZMAN------------------
+
+
+INSERT INTO "aranzman"("id","datum_realizacije","placeno","ukupna_cena","hotel","turisticka_agencija")
+VALUES (nextval('aranzman_seq'), to_date('01.03.2023.', 'dd.mm.yyyy.'),true,100000, 1, 1);
+
+
+INSERT INTO "aranzman"("id", "ukupna_cena", "placeno","datum_realizacije","hotel","turisticka_agencija")
+VALUES (nextval('aranzman_seq'), 45000, false, to_date('09.12.2023.', 'dd.mm.yyyy.'), 2, 2);
+
+
+INSERT INTO "aranzman"("id", "ukupna_cena", "placeno","datum_realizacije","hotel","turisticka_agencija")
+VALUES (nextval('aranzman_seq'), 23000, true, to_date('05.03.2023.', 'dd.mm.yyyy.'), 3, 3);
+
+
+INSERT INTO "aranzman"("id", "ukupna_cena", "placeno","datum_realizacije","hotel","turisticka_agencija")
+VALUES (nextval('aranzman_seq'), 105000, true, to_date('01.08.2022.', 'dd.mm.yyyy.'), 4, 4);
+
+
+INSERT INTO "aranzman"("id", "ukupna_cena", "placeno","datum_realizacije","hotel","turisticka_agencija")
+VALUES (nextval('aranzman_seq'), 15900, true, to_date('08.07.2023.', 'dd.mm.yyyy.'), 5, 5);
+
+
+INSERT INTO "aranzman"("id", "ukupna_cena", "placeno","datum_realizacije","hotel","turisticka_agencija")
+VALUES (nextval('aranzman_seq'), 45000, false, to_date('15.06.2023.', 'dd.mm.yyyy.'), 6, 6);
+
+
+INSERT INTO "aranzman"("id", "ukupna_cena", "placeno","datum_realizacije","hotel","turisticka_agencija")
+VALUES (nextval('aranzman_seq'), 23400, false, to_date('03.07.2023.', 'dd.mm.yyyy.'), 7, 7);
+
+
+INSERT INTO "aranzman"("id", "ukupna_cena", "placeno","datum_realizacije","hotel","turisticka_agencija")
+VALUES (nextval('aranzman_seq'), 20000, true, to_date('09.08.2023.', 'dd.mm.yyyy.'), 8, 1);
+
+
+INSERT INTO "aranzman"("id", "ukupna_cena", "placeno","datum_realizacije","hotel","turisticka_agencija")
+VALUES (nextval('aranzman_seq'), 670000, false, to_date('12.12.2023.', 'dd.mm.yyyy.'), 9, 2);
+
+
+INSERT INTO "aranzman"("id", "ukupna_cena", "placeno","datum_realizacije","hotel","turisticka_agencija")
+VALUES (nextval('aranzman_seq'), 12300, true, to_date('06.05.2023.', 'dd.mm.yyyy.'), 10, 3);
+
+
+INSERT INTO "aranzman"("id", "ukupna_cena", "placeno","datum_realizacije","hotel","turisticka_agencija")
+VALUES (nextval('aranzman_seq'), 78000, true, to_date('09.09.2023.', 'dd.mm.yyyy.'), 11, 4);
+
+
+INSERT INTO "aranzman"("id", "ukupna_cena", "placeno","datum_realizacije","hotel","turisticka_agencija")
+VALUES (nextval('aranzman_seq'), 100000, false, to_date('01.03.2023.', 'dd.mm.yyyy.'), 12, 5);
 
 
 
 
-
-/*
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 1, 1, 1, 20, 'komad', 100);
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 1, 2, 2, 30, 'komad', 150);
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 1, 3, 3, 15, 'komad', 500);
-
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 2, 1, 4, 30, 'komad', 1000);
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 2, 2, 5, 18, 'komad', 1300);
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 2, 3, 6, 20, 'komad', 500);
-
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 3, 1, 7, 30, 'kg', 200);
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 3, 2, 8, 50, 'kg', 80);
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 3, 3, 9, 25, 'kg', 130);
-
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 4, 1, 10, 5, 'kg', 300);
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 4, 2, 11, 2, 'kg', 500);
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 4, 3, 12, 3, 'kg', 400);
-
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 5, 1, 11, 10, 'kg', 300);
-INSERT INTO "stavka_porudzbine"("id", "porudzbina", "redni_broj", "artikal", "kolicina", "jedinica_mere", "cena")
-VALUES (nextval('stavka_porudzbine_seq'), 5, 2, 12, 10, 'kg', 500);*/
