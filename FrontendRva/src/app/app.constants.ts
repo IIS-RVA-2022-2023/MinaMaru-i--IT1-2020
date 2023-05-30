84eba5ee95bd8a15fa727507e6491f7043ddb7a8
marusicmina <minamarusic@gmail.com>
2023-05-30 22:37:44 +0200

    do ovog

--- FrontendRva/src/app/app.constants.ts
+++ FrontendRva/src/app/app.constants.ts
@@ -1,2 +1,3 @@
 export const TURAGENCIJA_URL='http://localhost:8080/turistickaAgencija';
 export const DESTINACIJA_URL='http://localhost:8080/destinacija';
+export const HOTEL_URL='http://localhost:8080/hotel';
